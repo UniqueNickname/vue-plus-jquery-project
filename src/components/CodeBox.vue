566e7f6034db271ebe016dc6c4b7c2bd90fc61a8
Kochura Ivan <dr.Art-47@yandex.ru>
2019-10-19 22:58:31 +0600

    add code box and code button

---
+++ src/components/CodeBox.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="code-box"
+    :class="{'code-box_active': codeIsShow}">
+    <div class="code-box-cancel"
+      @click="showCode"><span></span></div>
+      <slot></slot>
+  </div>
+</template>
+
+<script>
+import { eventEmitter } from '../main'
+
+export default {
+  data () {
+    return {
+      codeIsShow: false
+    }
+  },
+  methods: {
+    showCode () {
+      this.codeIsShow = !this.codeIsShow
+    }
+  },
+  created () {
+    eventEmitter.$on('codeUpdated', () => {
+      this.showCode()
+    })
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+@import '../assets/sass/code-box.sass'
+</style>
---
+++ src/components/CodeBtn.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="code-btn"
+    @click="showCode">
+    <span class="code-btn__line code-btn__line_top"></span>
+    <span class="code-btn__line code-btn__line_right"></span>
+    <span class="code-btn__line code-btn__line_bottom"></span>
+    <span class="code-btn__line code-btn__line_left"></span>
+    <div>Show code</div>
+  </div>
+</template>
+
+<script>
+import { eventEmitter } from '../main'
+
+export default {
+  methods: {
+    showCode () {
+      eventEmitter.$emit('codeUpdated')
+    }
+  }
+}
+</script>
+
+<style lang="sass" scoped>
+@import '../assets/sass/code-btn.sass'
+</style>
