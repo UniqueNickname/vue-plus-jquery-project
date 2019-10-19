289b9ee85099c96ba27c0ec2a144032a2d422469
Kochura Ivan <dr.Art-47@yandex.ru>
2019-10-19 20:08:04 +0600

    add images and PreviewList in ChangingImage

---
+++ src/components/PreviewList.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="preview-list">
+    <a v-for="(imgSource, index) in imgSources"
+      :key="index"
+      :href="imgSource + '_h.jpg'"
+      class="preview-list-item"
+      target="blank">
+      <img :src="imgSource + '.jpg'"
+        alt="preview"
+      />
+    </a>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['imgSources']
+}
+</script>
--- src/views/ChangeImage.vue
+++ src/views/ChangeImage.vue
@@ -3,19 +3,36 @@
     class="content change-image"
     :class="{'content_active' : contentIsActive}">
     <div class="header">Header</div>
+    <PreviewList :imgSources="imgSources"/>
   </div>
 </template>
 
 <script>
 import { eventEmitter } from '../main'
+import PreviewList from '@/components/PreviewList.vue'
 
 export default {
   name: 'home',
   components: {
+    PreviewList
   },
   data () {
     return {
-      contentIsActive: false
+      contentIsActive: true,
+      imgSources: [
+        './img/small/1',
+        './img/small/2',
+        './img/small/3',
+        './img/small/4',
+        './img/small/5',
+        './img/small/6',
+        './img/small/7',
+        './img/small/8',
+        './img/small/9',
+        './img/small/10',
+        './img/small/11',
+        './img/small/12'
+      ]
     }
   },
   methods: {
